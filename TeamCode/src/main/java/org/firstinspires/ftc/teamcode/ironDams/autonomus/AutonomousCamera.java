a9270c0a56e58816bf33b2b5af87b20e7dfb1d46
Joe <joe@talimaker.com>
2025-09-14 18:57:25 -0400

    Added more telemetry to AprilTagReader and Created the Ball Vision code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironDams/autonomus/AutonomousCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironDams/autonomus/AutonomousCamera.java
@@ -11,20 +11,23 @@ import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.ironDams.autonomus.subsystems.AprilTagReader;
 
 @Config
-@Autonomous(name = "AUTONOMOUS_CAMERA", group = "Autonomous")
+//@Autonomous(name = "AUTONOMOUS_CAMERA", group = "Autonomous")
+@TeleOp(name = "AUTONOMOUS_CAMERA", group = "IronDams")
 public class AutonomousCamera extends LinearOpMode {
 
-    private final AprilTagReader aprilTagReader = new AprilTagReader(this);
+
 //    private final Pose2d initPose = new Pose2d(0,0,0);
 //    private final MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
 
     @Override
     public void runOpMode() {
+        AprilTagReader aprilTagReader = new AprilTagReader(this, true);
 
         waitForStart();
 
@@ -32,9 +35,33 @@ public class AutonomousCamera extends LinearOpMode {
 
         Actions.runBlocking(
             new SequentialAction(
-                aprilTagReader.readTagAction()
+                aprilTagReader.readTagAction(),
+                // sort balls in intake. pass seq. # in as argument, aprilTagReader.getSequenceCode();
+                // shoot balls into hoper.
+                // drive to first pickup location.
+                new ParallelAction(
+                    // drive to shoot location
+                    // sort balls in intake
+                ),
+                // shoot balls into hoper.
+                // drive to second pickup location.
+                new ParallelAction(
+                    // drive to shoot location
+                    // sort balls in intake
+                ),
+                // shoot balls into hoper.
+                // drive to third pickup location.
+                new ParallelAction(
+                    // drive to shoot location
+                    // sort balls in intake
+                )
+                // shoot balls into hoper.
             )
         );
-    }
 
+//        telemetry.addData("Sequence Code", aprilTagReader.getSequenceCode());
+//        telemetry.update();
+
+        sleep(20000);
+    }
 }
