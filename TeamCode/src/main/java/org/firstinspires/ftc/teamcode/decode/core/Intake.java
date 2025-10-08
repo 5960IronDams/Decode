c5471e8fc6c086454065ab43312edf7a4c767e00
TheGoomyMan <delbridgeluke6@gmail.com>
2025-10-08 18:54:34 -0400

    Buttons for drive pract.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/core/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/core/Intake.java
@@ -4,21 +4,40 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
 
     private final DcMotorEx _motor;
+    private final LinearOpMode _opMode;
+    private boolean _runningIntake;
 
-    public Intake(HardwareMap hardwareMap) {
-        _motor = hardwareMap.get(DcMotorEx.class, "intake");
+    public Intake(LinearOpMode opMode) {
+        _opMode = opMode;
+        _motor = opMode.hardwareMap.get(DcMotorEx.class, "intake");
 
         _motor.setDirection(DcMotorEx.Direction.REVERSE);
         _motor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         _motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
     }
 
+    public boolean isRunning(){
+        return _runningIntake;
+    }
+
+    private Intake toggleIntake(){
+        if(_opMode.gamepad2.b) {
+            _runningIntake = !_runningIntake;
+
+            if (_runningIntake) setPower(1);
+            else stop();
+            _opMode.sleep(100);
+        }
+
+        return this;
+    }
     public void setPower(double power) {
         _motor.setPower(power);
     }
@@ -32,7 +51,7 @@ public class Intake {
                     initialized = true;
                 }
 
-                setPower(0.15);
+                toggleIntake();
 
                 return true;
             }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/core/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/core/Spindexer.java
@@ -4,18 +4,44 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.hardware.digitalchickenlabs.OctoQuad;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import org.firstinspires.ftc.teamcode.ironDams.core.OctoQuadFWv3;
+
+import java.util.Objects;
 
 public class Spindexer {
 
     private final CRServo _spindexer;
+    private final DcMotorEx enc;
+    private final ColorVision _colorVision;
+    private final Decoder _decoder;
+    private final LinearOpMode _opMode;
+    private Intake _intake;
+    private Launcher _launcher;
 
-    public Spindexer(HardwareMap hardwareMap) {
-        _spindexer = hardwareMap.get(CRServo.class, "spindex");
+    private int previousRotation = 0;
+
+    public Spindexer(LinearOpMode opMode, Decoder decoder, ColorVision colorVision, Intake intake, Launcher launcher) {
+        _launcher = launcher;
+        _intake = intake;
+        _opMode = opMode;
+        _colorVision = colorVision;
+        _decoder = decoder;
+        _spindexer = opMode.hardwareMap.get(CRServo.class, "spindex");
+        enc = opMode.hardwareMap.get(DcMotorEx.class, "enc");
+        enc.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        previousRotation = readPos();
         closeLauncher();
     }
-
     public void openLauncher() {
         double openPos = 1.0;
     }
@@ -28,6 +54,23 @@ public class Spindexer {
         setPower(1.0);
     }
 
+    private Spindexer toggleSpindexer(){
+
+        if (_intake.isRunning()){
+            setPower(0.25);
+        }
+        else if (_launcher.isRunning()){
+            setPower(0.15);
+        }
+        else{
+            stop();
+        }
+        return this;
+    }
+    public int readPos(){
+        int y = enc.getCurrentPosition();
+        return y / 2731;
+    }
     public void setPower(double power) {
         _spindexer.setPower(power);
     }
@@ -45,8 +88,29 @@ public class Spindexer {
                 if (!initialized) {
                     initialized = true;
                 }
+                toggleSpindexer();
 
-                setPower(0.10);
+//                int currentRotation = readPos();
+//                String targetPattern = _decoder.getSequenceCodeString();
+//                String currentPattern = "UUU";
+//
+//                if (previousRotation != currentRotation) {
+//                    stop();
+//                    previousRotation = currentRotation;
+//                    currentPattern = _colorVision.getPattern();
+//
+//                    if (targetPattern.equalsIgnoreCase(currentPattern)) {
+//                        stop();
+//                    }
+//                    else {
+//                        setPower(0.7);
+//                    }
+//                }
+//                packet.put("Target Pattern", targetPattern);
+//                packet.put("Pattern Currently", currentPattern);
+//
+//                packet.put("Match", Objects.equals(targetPattern, currentPattern));
+//                packet.put("Y", readPos());
 
                 return true;
             }
