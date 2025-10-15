3b92355c66cb9d5fdb20f7b6ec6a0c70eb9369c4
Mason-Johnson2012 <johnsonmillermason@gmail.com>
2025-10-15 19:28:32 -0400

    adding Acceleration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironDams/autonomus/subsystems/Drive.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.ironDams.autonomus.subsystems;
+
+public class Drive {
+    public void moveTo(double maxPow,double startPow,int targetPos, int startPos,int accelTo, int deccelAt)
+    {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironDams/core/Acceleration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironDams/core/Acceleration.java
@@ -1,11 +1,23 @@
 package org.firstinspires.ftc.teamcode.ironDams.core;
 
 public final class Acceleration {
-    public static double getPower(int targetPos, int currentPos, int startDeceleration, double maxPower) {
-//        double decel = targetPos - startDeceleration;
-        double distance = targetPos - currentPos;
+//    public static double getPower(int targetPos, int currentPos, int startDeceleration, double maxPower) {
+////        double decel = targetPos - startDeceleration;
+//        double distance = targetPos - currentPos;
+//
+//        if (distance > startDeceleration) return maxPower;
+//        else return Math.max(-maxPower, Math.min(maxPower, Math.sqrt(distance / startDeceleration)));
+//    }
 
-        if (distance > startDeceleration) return maxPower;
-        else return Math.max(-maxPower, Math.min(maxPower, Math.sqrt(distance / startDeceleration)));
+    public static double getPower(double maxPow,double startPow,int targetPos, int currentPos, int startPos,int accelTo, int deccelAt)
+    {
+        int decelDist = targetPos-deccelAt;
+        if (currentPos > decelDist){
+            int actualDist =  targetPos - currentPos;
+            double deccelPerc = (double)actualDist / (double)decelDist;
+            return maxPow * deccelPerc;
+        } else {
+         return 0;
+        }
     }
 }
