c260d51e659d00520007fae55ac393957d0113a9
Joe <joe@talimaker.com>
2025-10-28 17:34:23 -0400

    Added velocity tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/VelocityTunerOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/VelocityTunerOpMode.java
@@ -1,28 +1,40 @@
 package org.firstinspires.ftc.teamcode.decode.teleOp;
 
-import com.acmerobotics.roadrunner.InstantAction;
-import com.acmerobotics.roadrunner.ParallelAction;
-import com.acmerobotics.roadrunner.SequentialAction;
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.teamcode.decode.Constants;
-import org.firstinspires.ftc.teamcode.decode.core.GreenBallPosition;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @TeleOp(name = "VelocityTuner", group = "@@@@IronDams")
 public class VelocityTunerOpMode extends LinearOpMode {
+    private int activeMotorIndex = 0;
+    private DcMotorEx activeMotor;
+    private List<Map.Entry<String, DcMotorEx>> entries;
+    private final double INCREMENT = 100;
+    private double targetVelocity = 0;
+    private double rightTicksPerSec = 2245;
+    private double leftTicksPerSec = 2305;
+    private VoltageSensor batteryVoltageSensor;
+
     @Override
     public void runOpMode() throws InterruptedException {
+
+        batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
 
         DcMotorEx dmfl  = hardwareMap.get(DcMotorEx.class, "leftFront");
         DcMotorEx dmbl = hardwareMap.get(DcMotorEx.class, "leftBack");
@@ -43,28 +55,27 @@ public class VelocityTunerOpMode extends LinearOpMode {
         motors.put("Right Shooter", mrs);
         motors.put("Intake", mi);
 
-        int activeMotorIndex = 0;
-        DcMotorEx activeMotor = motors.get(activeMotorIndex);
-
-        List<Map.Entry<String, DcMotorEx>> entries = new ArrayList<>(motors.entrySet());
+        entries = new ArrayList<>(motors.entrySet());
         activeMotor = entries.get(activeMotorIndex).getValue();
 
-        dmfl.setDirection(DcMotor.Direction.FORWARD);
-        dmbl.setDirection(DcMotor.Direction.FORWARD);
-        dmbr.setDirection(DcMotor.Direction.REVERSE);
-        dmfr.setDirection(DcMotor.Direction.REVERSE);
+        dmfl.setDirection(DcMotorEx.Direction.FORWARD);
+        dmbl.setDirection(DcMotorEx.Direction.FORWARD);
+        dmbr.setDirection(DcMotorEx.Direction.REVERSE);
+        dmfr.setDirection(DcMotorEx.Direction.REVERSE);
+
+        mls.setDirection(DcMotorEx.Direction.REVERSE);
         mi.setDirection(DcMotorEx.Direction.REVERSE);
 
         for (Map.Entry<String, DcMotorEx> motor : motors.entrySet()) {
-            motor.getValue().setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor.getValue().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            motor.getValue().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.getValue().setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            motor.getValue().setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            motor.getValue().setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         }
 
-        dmfl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        dmbl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        dmbr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        dmfr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        dmfl.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        dmbl.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        dmbr.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        dmfr.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
         while(!isStopRequested() && !opModeIsActive()) { }
 
@@ -78,26 +89,91 @@ public class VelocityTunerOpMode extends LinearOpMode {
             return;
         }
 
-        while (opModeIsActive()) {
+        Actions.runBlocking(tuneAction());
+    }
 
-            if (gamepad1.left_trigger != 0 && activeMotorIndex > 0)
-            {
-                activeMotorIndex--;
-                activeMotor = entries.get(activeMotorIndex).getValue();
-                sleep(250);
-            } else if (gamepad1.right_trigger != 0 && activeMotorIndex < (entries.size() - 1)) {
-                activeMotorIndex++;
-                activeMotor = entries.get(activeMotorIndex).getValue();
-                sleep(250);
+    public Action tuneAction() {
+        return new Action() {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    initialized = true;
+                }
+
+                if (gamepad1.left_trigger != 0 && activeMotorIndex > 0)
+                {
+                    activeMotorIndex--;
+                    activeMotor = entries.get(activeMotorIndex).getValue();
+                    sleep(250);
+                } else if (gamepad1.right_trigger != 0 && activeMotorIndex < (entries.size() - 1)) {
+                    activeMotorIndex++;
+                    activeMotor = entries.get(activeMotorIndex).getValue();
+                    sleep(250);
+                }
+
+                String activeMotorName = entries.get(activeMotorIndex).getKey();
+                double f = 0;
+                double d = .0005;
+                PIDFCoefficients pidf = new PIDFCoefficients(0,0,0,0);
+                if (activeMotorName.equals("Left Shooter")) {
+                    f = (32767 / leftTicksPerSec) * 12.0 / batteryVoltageSensor.getVoltage();
+                } else if (activeMotorName.equals("Right Shooter")) {
+                    f = (32767 / rightTicksPerSec) * 12.0 / batteryVoltageSensor.getVoltage();
+                }
+
+                if (f != 0) pidf = new PIDFCoefficients(0, 0, d, f);
+
+                if (gamepad1.a) {
+                    if (targetVelocity > 0) {
+                        activeMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
+
+                        targetVelocity -= INCREMENT;
+                        activeMotor.setVelocity(targetVelocity);
+                    }
+                    sleep(250);
+                } else if (gamepad1.y) {
+                    activeMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
+
+                    targetVelocity += INCREMENT;
+                    activeMotor.setVelocity(targetVelocity);
+                    sleep(250);
+                } else if (gamepad1.x) {
+                    // Gets ticks per second
+                    activeMotor.setPower(1);
+                    sleep(250);
+                } else if (gamepad1.b) {
+                    activeMotor.setPower(0);
+                    sleep(250);
+                } else if (gamepad1.dpad_left) {
+                    if (activeMotorName.equals("Left Shooter")) {
+                        leftTicksPerSec -= 5;
+                    } else if (activeMotorName.equals("Right Shooter")) {
+                        rightTicksPerSec -= 5;
+                    }
+                    sleep(250);
+                } else if (gamepad1.dpad_right) {
+                    if (activeMotorName.equals("Left Shooter")) {
+                        leftTicksPerSec += 5;
+                    } else if (activeMotorName.equals("Right Shooter")) {
+                        rightTicksPerSec += 5;
+                    }
+                    sleep(250);
+                }
+
+                packet.put("Power", activeMotor.getPower());
+                packet.put("Velocity A", activeMotor.getVelocity());
+                packet.put("Velocity T", targetVelocity);
+                packet.put("Active Motor", activeMotorName);
+                packet.put("TPS-L", leftTicksPerSec);
+                packet.put("TPS-R", rightTicksPerSec);
+                packet.put("Error", targetVelocity - activeMotor.getVelocity());
+                packet.put("Voltage", batteryVoltageSensor.getVoltage());
+
+
+                return true;
             }
-
-            activeMotor.setPower(gamepad1.left_stick_y);
-
-            telemetry.addData("Active Motor", entries.get(activeMotorIndex).getKey());
-            telemetry.addData("Current Position", activeMotor.getCurrentPosition());
-            telemetry.addData("Velocity", activeMotor.getVelocity());
-            telemetry.update();
-
-        }
+        };
     }
 }
